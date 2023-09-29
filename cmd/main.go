ea66990fda142c7739a8ede076a9db99428dfbed
jmfinch <jeremy.finch@officeworksoftware.com>
2023-09-29 07:58:53 -0600

    fix(cmd): Use repository
    added repository after adding encapsulation

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/jmfinch/lotrsdk/src/character"
 	"github.com/jmfinch/lotrsdk/src/movie"
-	"github.com/jmfinch/lotrsdk/src/quote"
-	"github.com/jmfinch/lotrsdk/src/request"
+
 )
 
 func main() {
@@ -14,7 +12,10 @@ func main() {
 }
 
 func testMovies() {
-	movies, err := movie.GetMovies()
+	// Create a repository instance using NewMovieRepository()
+	movieRepo := movie.NewMovieRepository()
+
+	movies, err := movieRepo.GetMovies()
 	if err != nil {
 		log.Println(err)
 	}
@@ -22,54 +23,14 @@ func testMovies() {
 		log.Println(movie.ID, movie.Name)
 	}
 
-	TwoTowers, err := movie.GetMovieByID("5cd95395de30eff6ebccde5b")
+	TwoTowers, err := movieRepo.GetMovieByID("5cd95395de30eff6ebccde5b")
 	if err != nil {
 		log.Println(err)
 	}
 	log.Println(TwoTowers.Name, TwoTowers.BudgetInMillions)
-	ReturnOftheKing, err := movie.GetMovieByName("The Return of the King")
+	ReturnOftheKing, err := movieRepo.GetMovieByName("The Return of the King")
 	if err != nil {
 		log.Println(err)
 	}
 	log.Println(ReturnOftheKing.Name, ReturnOftheKing.AcademyAwardWins)
-}
-
-func testQuotes() {
-	//Getting all quotes
-	quotes, err := quote.GetQuotes(
-		request.WithSort("character", "asc"),
-		request.WithLimit(10),
-		request.WithPage(3),
-	)
-	if err != nil {
-		log.Println(err)
-	}
-	for _, quote := range quotes {
-		char, err := character.GetCharacterByID(quote.CharacterID)
-		if err != nil {
-			log.Println("ooof")
-		}
-		log.Println(char.Name, quote.Dialog)
-	}
-
-}
-
-func quoteHolder() {
-	movieId := "5cd95395de30eff6ebccde5b"
-	testQuotes, err := quote.GetQuotesFromMovie(movieId)
-	if err != nil {
-		log.Println(err)
-	}
-	for _, quote := range testQuotes {
-		log.Println(quote.ID, quote.Dialog)
-	}
-
-	//Get Quote by ID
-	quoteId := "5cd96e05de30eff6ebccebb1"
-	testQuote, err := quote.GetQuoteByID(quoteId)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println(testQuote.Dialog, testQuote.ID)
-	//quoteDialog := "Hey, stinker, don't go gettingtoo far ahead."
 }
